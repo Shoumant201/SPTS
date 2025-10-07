3af387748550c13c4a72fecfa07b3886b0da6621
Shoumant201 <np03cs4s240193@heraldcollege.edu.np>
2025-10-07 19:20:00 +0545

    Add user schema and DB models

---
+++ backend/src/utils/password.ts
@@ -0,0 +1,15 @@
+import * as bcrypt from 'bcryptjs';
+import * as crypto from 'crypto';
+
+export const hashPassword = async (password: string): Promise<string> => {
+  const saltRounds = 12;
+  return bcrypt.hash(password, saltRounds);
+};
+
+export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
+  return bcrypt.compare(password, hashedPassword);
+};
+
+export const generateResetToken = (): string => {
+  return crypto.randomBytes(32).toString('hex');
+};
