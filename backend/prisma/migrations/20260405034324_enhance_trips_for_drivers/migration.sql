06c316a57ef0cb5f54fe1257be0b2a38505c1188
Shoumant201 <np03cs4s240193@heraldcollege.edu.np>
2026-05-31 00:02:05 +0545

    fix: remove duplicate migration causing deployment failure
    - Removed 20260405034324_enhance_trips_for_drivers migration
    - This migration was trying to set passengerCount as NOT NULL before the column existed
    - The correct migration 20260405050000_enhance_trips_for_drivers adds the column with default value
    - This fixes the Render deployment migration failure

--- backend/prisma/migrations/20260405034324_enhance_trips_for_drivers/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `passengerCount` on table `trips` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "trips" ALTER COLUMN "passengerCount" SET NOT NULL;
